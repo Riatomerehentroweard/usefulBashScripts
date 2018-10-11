ebf3d06f8b607cd3e86ca30c2d5fc3afe4d7fb3a
Daniel Haeusler <daniel.haeusler@3ap.ch>
2018-10-11 22:41:11 +0200

    added files

--- allThemFiles.sh
+++ allThemFiles.sh
@@ -1,4 +1,11 @@
-echo "Here you have all the files:"
+echo "Here you have all the file in the current direcotry:"
+for f in ./*; do
+    echo $f
+done
+
+printf "
+And now all the files in a subfolder:
+"
 for f in ./allThemFiles/*; do
     echo $f
 done
@@ -9,4 +16,28 @@ And now just the doc files:
 
 for f in ./allThemFiles/*.doc; do
     echo $f
+done
+
+printf "
+Now let's move some files. Currently these files are in cup a:
+"
+
+for f in ./allThemFiles/cup_a/*; do
+    echo $f
+done
+
+printf "
+Cup b has the following files:
+"
+
+for f in ./allThemFiles/cup_b/*; do
+    echo $f
+done
+
+printf "
+moving all the files from a to b.
+"
+
+for f in ./allThemFiles/cup_a/*; do
+    mv $f ./../cup_b
 done
