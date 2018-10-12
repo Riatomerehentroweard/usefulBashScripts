107f287696e2cdbbc090c4cbdbea7bfd0041c2f7
Daniel Haeusler <daniel.haeusler@3ap.ch>
2018-10-12 08:52:20 +0200

    initial script to create test data and find the video files

--- showMeThemBiggies.sh
+++ showMeThemBiggies.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-for f in ./allThemFiles/cup_b/*; do
-    echo $f
-done
+printf "cleaning up files from previous test...
+"
+rm -rf themVidFiles
+
+printf "creating files for test...
+"
+mkdir themVidFiles themVidFiles/movieA themVidFiles/movieB themVidFiles/tvShowA themVidFiles/tvShowB
+
+touch themVidFiles/movieA/movieA.2009.mp4 themVidFiles/movieB/movieB_2010.mkv themVidFiles/tvShowA/tvShowA.s03e04.avi themVidFiles/tvShowB/tvShowB.S02E02.flv themVidFiles/movieA/movieA.sub.srt themVidFiles/movieA/license.txt themVidFiles/tvShowA/a.sub.srt themVidFiles/tvShowB/garbage.file
+
+printf "
+Following video files can be found:
+
+"
+find themVidFiles -name '*.mkv' -or -name '*.mp4' -or -name '*.avi' -or -name '*.flv'
+
+#for f in ./allThemFiles/cup_b/*; do
+#    echo $f
+#done
